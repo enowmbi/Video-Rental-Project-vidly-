dbe1da4dc1cff0d30873a0e7f86c217dda75677d
Enow Mbi <benowmbi@yahoo.com>
2019-03-25 13:15:40 +0100

    update movies add key and update columns

--- src/components/movies.js
+++ src/components/movies.js
@@ -3,20 +3,50 @@ import { getMovies } from '../services/fakeMovieService';
 
 
 class Movies extends Component{
- state ={
-  movies: getMovies()
- }
+    state ={
+        movies: getMovies()
+    }
+
+    handleClick=(id)=>{
+        const movies = this.state.movies.filter(movie => movie._id !== id)
+        this.setState({movies: movies})
+    }
 
     render(){
-     return(
-         <div>
-             <p> Showing {this.state.movies.length} movies in the database</p>
+        if(this.state.movies.length ===0){
 
-            
-     </div>
-     )
-    
-    
+            return <div className="container"><p className="m-4">There are no movies in the database.</p></div>
+        }
+        return(
+            <div className="container">
+                <p className="m-4"> Showing {this.state.movies.length} {this.state.movies.length > 1? "movies" :"movie"} in the database.</p>
+                <table className="table">
+                    <thead>
+                        <tr>
+                            <th>Title</th>
+                            <th>Genre</th>
+                            <th>Qty in Stock</th>
+                            <th>Rate</th>
+                            <th/>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {this.state.movies.map(movie =>(
+                            <tr key={movie._id}>
+                                <td>{movie.title}</td>
+                                <td>{movie.genre.name}</td>
+                                <td>{movie.numberInStock}</td>
+                                <td>{movie.dailyRentalRate}</td>
+                                <td><button className="btn btn-danger btn-sm m-2" onClick={()=>this.handleClick(movie._id)}>Delete</button> </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+
+            </div>
+        )
+
+
     }
 
 
