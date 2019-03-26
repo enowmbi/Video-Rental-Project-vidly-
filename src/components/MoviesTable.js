40319014033ab47b833c037778e4f88e9748b913
Enow Mbi <benowmbi@yahoo.com>
2019-03-26 19:59:13 +0100

    extract tableheader into separate component

--- src/components/MoviesTable.js
+++ src/components/MoviesTable.js
@@ -1,36 +1,29 @@
 import React,{ Component } from 'react';
 import Like from './common/Like';
+import TableHeader from './common/TableHeader';
+
 
 class MoviesTable  extends Component{
 
-    raiseSort=(column)=>{
-      console.log(column)
-        const sortColumn = {...this.props.sortColumn}
-        if(column === sortColumn.column){
-            sortColumn.order = (sortColumn.order ==='asc'?'desc':'asc')            
-        }else
-        {
-            sortColumn.column = column
-            sortColumn.order = 'asc'
-        }
-      this.props.onSort(sortColumn) 
-    }
-
+    columns =[
+        {name:'title',label:'Title'},     
+        {name:'genre.name',label:'Genre'},      
+        {name:'numberInStock',label:'Qty in Stock'},      
+        {name:'dailyRentalRate',label:'Rate'},      
+        { },      
+        { }      
+    ]
 
     render(){
         return(
         <React.Fragment>
         <table className="table">
-            <thead className="App-clickable">
-                <tr>
-                    <th onClick={()=>this.raiseSort('title')}>Title</th>
-                    <th onClick={()=>this.raiseSort('genre.name')}>Genre</th>
-                    <th onClick={()=>this.raiseSort('numberInStock')}>Qty in Stock</th>
-                    <th onClick={()=>this.raiseSort('dailyRentalRate')}>Rate</th>
-                    <th/>
-                    <th/>
-                </tr>
-            </thead>
+            <TableHeader 
+                columns={this.columns}
+                sortColumn={this.props.sortColumn}
+                onSort={this.props.onSort}
+            />
+
             <tbody>
                 {this.props.movies.map(movie =>(
                     <tr key={movie._id}>
---
+++ src/components/common/TableHeader.js
@@ -0,0 +1,31 @@
+import React,{ Component } from 'react';
+
+
+class TableHeader extends Component{
+
+    raiseSort=(column)=>{
+      console.log(column)
+        const sortColumn = {...this.props.sortColumn}
+        if(column === sortColumn.column){
+            sortColumn.order = (sortColumn.order ==='asc'?'desc':'asc')            
+        }else
+        {
+            sortColumn.column = column
+            sortColumn.order = 'asc'
+        }
+      this.props.onSort(sortColumn) 
+    }
+
+    render(){
+        return(
+            <thead className="App-clickable">
+                <tr>
+                {this.props.columns.map(column =>
+                        <th onClick={()=>this.raiseSort(column.name)}>{column.label}</th>
+                )}
+            </tr>
+            </thead>
+        )}
+}
+
+export default TableHeader
