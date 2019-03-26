79148d47ce39fad1a2055fdc8d8bd8355c1128f9
Enow Mbi <benowmbi@yahoo.com>
2019-03-26 20:59:21 +0100

    add unique key to tablebody

--- src/components/common/TableBody.js
+++ src/components/common/TableBody.js
@@ -14,7 +14,6 @@ class TableBody extends Component{
                 <td>{item.dailyRentalRate}</td>
                 <td><Like movie={item} onLike={this.props.onLike}/></td>
                 <td><button className="btn btn-danger btn-sm m-2" onClick={()=>this.props.onDelete(item._id)}>Delete</button></td>
-
             </tr>
             
             
