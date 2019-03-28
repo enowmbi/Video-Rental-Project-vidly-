75f20934a83684df47401b9784f05363252aaa21
Enow Mbi <benowmbi@yahoo.com>
2019-03-28 21:45:19 +0100

    add MovieForm for movie details displaying id of movie

---
+++ src/components/MovieForm.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const MovieForm =(props)=>{
+    //const MovieForm=({ match, history })=>{
+    
+
+    return(
+        <div>
+            <h1>Movie Form {props.match.params.id} </h1>
+            <button className="btn btn-primary" onClick={()=> props.history.push('/movies')}>Save</button>
+        </div>
+    )
+}
+
+export default MovieForm
+
