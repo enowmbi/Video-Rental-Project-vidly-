f93387fc5bb531830bf5c9ac3969faee82838980
Enow Mbi <benowmbi@yahoo.com>
2019-03-28 22:08:04 +0100

    add login form

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Customers from './components/Customers';
 import Rentals from './components/Rentals';
 import NotFound from './components/NotFound';
 import MovieForm from './components/MovieForm';
+import Login from './components/Login';
 import { Route,Switch,Redirect } from 'react-router-dom';
 import './App.css';
 
@@ -21,6 +22,7 @@ class App extends Component {
                         <Route path="/customers" component={Customers} />
                         <Route path="/Rentals"  component={Rentals} />
                         <Route path="/not-found" component={NotFound} />
+                        <Route path="/login" component={Login} />
                         <Redirect from="/" exact  to="/movies" />
                         <Redirect to="/not-found" />
                     </Switch>
