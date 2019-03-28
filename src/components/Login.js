681d79ecf3baef43fdf4cea89ba3c9cc9f30690e
Enow Mbi <benowmbi@yahoo.com>
2019-03-28 22:25:11 +0100

    create bootstrap login form

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,14 +2,32 @@ import React, { Component } from 'react';
 
 
 class Login extends Component{
+    handleSubmit=(e)=>{
+        e.preventDefault();
+
+        console.log("submitted")
+    }
 
     render(){
         return(
-            <h1>Login</h1>
-        
+            <div className="container">
+                <h1>Login</h1>
+                <form onSubmit={this.handleSubmit}>
+                    <div className="form-group">
+                        <label htmlFor="exampleInputEmail1">Email address</label>
+                        <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
+                        <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="exampleInputPassword1">Password</label>
+                        <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" />
+                    </div>
+                    <button type="submit" className="btn btn-primary">Submit</button>
+                </form>
+            </div>
         )
-    
+
     }
 
 }
- export default Login
+export default Login
