5bb0c67c1eca76e333f77fd8169e8a825e5f8a5f
Enow Mbi <benowmbi@yahoo.com>
2019-03-28 21:43:17 +0100

    add reactfragment around components and div for the content area

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
 import React, { Component } from 'react';
 import Movies from './components/Movies';
 import NavBar from  './components/NavBar';
+import Customers from './components/Customers';
+import Rentals from './components/Rentals';
+import NotFound from './components/NotFound';
+import MovieForm from './components/MovieForm';
+import { Route,Switch,Redirect } from 'react-router-dom';
 import './App.css';
+
 
 class App extends Component {
     render() {
         return (
             <React.Fragment>
                 <NavBar />
-                <Movies />
+                <div className="container">
+                    <Switch>
+                        <Route path="/movies/:id" component={MovieForm} />
+                        <Route path="/movies" component={Movies}  />
+                        <Route path="/customers" component={Customers} />
+                        <Route path="/Rentals"  component={Rentals} />
+                        <Route path="/not-found" component={NotFound} />
+                        <Redirect from="/" exact  to="/movies" />
+                        <Redirect to="/not-found" />
+                    </Switch>
+                </div>
             </React.Fragment>
         );
     }
