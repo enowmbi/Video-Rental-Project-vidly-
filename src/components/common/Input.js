8e90302af55ed0708abcd84ec52b723b7e8609d4
Enow Mbi <benowmbi@yahoo.com>
2019-03-29 19:22:08 +0100

    add <Input /> component

---
+++ src/components/common/Input.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+
+const Input =(props)=>{
+
+    return(
+        <div className="form-group">
+            <label htmlFor={props.name}>{props.label}</label>
+            <input type={props.type}
+                name={props.name}
+                className="form-control" 
+                value={props.value} 
+                onChange={props.onChange} 
+                id={props.name} 
+                placeholder={props.placeholder} 
+            />
+        </div>
+
+    )
+
+}
+export default Input
